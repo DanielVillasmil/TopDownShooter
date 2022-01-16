9e7ad88100ba50c3316e201046d8ec951c3fb7bf
DanielVillasmil <dvillasmil08@gmail.com>
2022-01-16 18:48:39 -0500

    bullets and zombies disappear after killing

--- main.lua
+++ main.lua
@@ -50,6 +50,28 @@ function love.update(dt)
             table.remove(bullets, i)
         end
     end
+
+    for i,z in ipairs(zombies) do
+        for j,b in ipairs(bullets) do
+            if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
+                z.dead = true
+                b.dead = true
+            end
+        end
+    end
+
+    for i=#zombies, 1, -1 do
+       local z = zombies[i]
+       if z.dead == true then
+            table.remove(zombies, i)
+       end
+    end
+    for i=#bullets, 1, -1 do
+        local z = bullets[i]
+        if z.dead == true then
+             table.remove(bullets, i)
+        end
+     end
 end
 
 function love.draw()
@@ -91,6 +113,7 @@ function spawnZombie()
     zombie.x = math.random(0, love.graphics.getWidth())
     zombie.y = math.random(0, love.graphics.getHeight())
     zombie.speed = 100
+    zombie.dead = false
     table.insert(zombies, zombie)
 end
 
@@ -99,6 +122,7 @@ function spawnBullet()
     bullet.x = player.x
     bullet.y = player.y
     bullet.speed = 500
+    bullet.dead = false
     bullet.direction = playerMouseAngle()
     table.insert(bullets, bullet)
 end
